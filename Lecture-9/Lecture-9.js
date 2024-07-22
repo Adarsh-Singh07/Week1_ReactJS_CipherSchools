14dd44c7482ce4bc786ebca13992b740782e7411
Adarsh Singh <adarsh2001gop@gmail.com>
2024-07-22 16:05:39 +0530

    first commit

---
+++ Lecture-9/Lecture-9.js
@@ -0,0 +1,36 @@
+import React from "react";
+const Test = {{ name = " ", setName }} => {
+    return {
+    <>
+      <h1>The name is : {name}</h1>
+      <input
+        type = "text"
+        value = "name"
+        onChange = {(e) => setName (e.target.value)}
+      />
+      </>
+    };
+}
+export default Test;
+
+____________________________________________________________________
+
+import React from "react";
+import Proptypes from "props-types";
+const Test = {{ name = " ", setName }} => {
+    return {
+    <>
+      <h1>The name is : {name}</h1>
+      <input
+        type = "text"
+        value = "name"
+        onChange = {(e) => setName (e.target.value)}
+      />
+      </>
+    };
+}
+Test.PropsTypes = {
+    name: PropsTypes.string,
+    setName: PropsTypes, 
+};
+export default Test;
